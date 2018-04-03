7bccd0dc7ffa0196ccd7c556ec48c6af14a2632a
Elina Rauhala <elina0rauhala@gmail.com>
2018-04-03 09:26:23 +0300

    Projektit luotu

---
+++ loppuprojekti_spring/src/main/java/fi/academy/loppuprojekti/LoppuprojektiApplication.java
@@ -0,0 +1,12 @@
+package fi.academy.loppuprojekti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoppuprojektiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoppuprojektiApplication.class, args);
+	}
+}
