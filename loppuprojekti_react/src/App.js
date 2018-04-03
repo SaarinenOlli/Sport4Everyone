71d656522b4fb561004206c27906d9eb5b84e50b
Tiinakooo <tiina.hrd@gmail.com>
2018-04-03 14:06:15 +0300

    lisätty etusivun perusjutut ja routing

--- loppuprojekti_react/src/App.js
+++ loppuprojekti_react/src/App.js
@@ -1,24 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-import Navi from './Navi';
+import Kirjaudu from './Kirjaudu';
+import Home from './Home';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-          <nav className="Navi">
-              <Navi/>
-          </nav>
-        <header className="App-header">
-          <h1 className="App-title">Training app</h1>
-        </header>
-        <p className="App-intro">
-            Tämä on treeniappi, johon voit syöttää omia treenitietoja, <br/> ja pääset vertailemaan omia treenituloksia!
-        </p>
-      </div>
-    );
-  }
-}
+const App = appProps => (
+    <Router>
+        <Switch>
+        <Route exact name="index" path="/" component={Home}/>
+        <Route exact path="/kirjaudu" component={Kirjaudu}/>
+        {/*<Route exact path="/profiili" component={Profile}/>*/}
+        </Switch>
+    </Router>
+);
 
 export default App;
---
+++ loppuprojekti_react/src/Kirjaudu.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+import {Modal, Button} from 'react-bootstrap';
+import './App.css';
+
+class Kirjaudu extends Component {
+    render() {
+        return (
+            <div className="static-modal">
+                <Modal.Dialog>
+                    <Modal.Header>
+                        <Modal.Title>Modal title</Modal.Title>
+                    </Modal.Header>
+
+                    <Modal.Body>One fine body...</Modal.Body>
+
+                    <Modal.Footer>
+                        <Button>Close</Button>
+                        <Button bsStyle="primary">Save changes</Button>
+                    </Modal.Footer>
+                </Modal.Dialog>
+            </div>
+        );
+    }
+}
+
+export default Kirjaudu;
+
+
