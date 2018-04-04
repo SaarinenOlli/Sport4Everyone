0a20f34e21cf205354285a06f251fc3b0e89cc08
Renne Brandt <renne.brandt@iki.fi>
2018-04-04 10:49:26 +0300

    profiledata-sivu lisätty

--- loppuprojekti_react/src/App.js
+++ loppuprojekti_react/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Home from './Home';
 import Profile from './Profile';
+import Profiledata from './Profiledata';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 const App = appProps => (
@@ -9,6 +10,7 @@ const App = appProps => (
         <Switch>
         <Route exact name="index" path="/" component={Home}/>
         <Route exact path="/profile" component={Profile}/>
+        <Route exact path="/profiledata" component={Profiledata}/>
         </Switch>
     </Router>
 );
