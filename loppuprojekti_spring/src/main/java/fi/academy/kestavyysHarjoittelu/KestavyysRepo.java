f32db182c883f7fff1b9424867513071ec7d878a
Elina Rauhala <elina0rauhala@gmail.com>
2018-04-09 12:04:15 +0300

    Uinnin, pyöräilyn ja juoksun mäppääminen erikseen

--- loppuprojekti_spring/src/main/java/fi/academy/kestavyysHarjoittelu/KestavyysRepo.java
+++ loppuprojekti_spring/src/main/java/fi/academy/kestavyysHarjoittelu/KestavyysRepo.java
@@ -1,8 +1,12 @@
 package fi.academy.kestavyysHarjoittelu;
 
+import fi.academy.kestavyysHarjoittelu.KestavyysHarjoittelu;
 import org.springframework.data.repository.CrudRepository;
 
 // by Heidi Ja Elina
 public interface KestavyysRepo extends CrudRepository<KestavyysHarjoittelu, Integer> {
 
+    Iterable<KestavyysHarjoittelu> findAllByLaji(String laji);
+
+    Iterable<KestavyysHarjoittelu> findAllByLajiAndKayttajaId(String laji, String id);
 }
